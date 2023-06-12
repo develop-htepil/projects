2fa2e0430d1554fb4ee602866c8e434d6123432c
Develop Htepil <136284604+develop-htepil@users.noreply.github.com>
2023-06-12 16:19:39 -0400

    System Finance 1.2
    System Finance Personal 1.0

--- script.js
+++ script.js
@@ -2,16 +2,22 @@ const tbody = document.querySelector("tbody");
 const descItem = document.querySelector("#desc");
 const amount = document.querySelector("#amount");
 const type = document.querySelector("#type");
+const date = document.querySelector("#date");
 const btnNew = document.querySelector("#btnNew");
+const btnSave = document.querySelector("#btnSave");
+const btnLoad = document.querySelector("#btnLoad");
 
 const incomes = document.querySelector(".incomes");
 const expenses = document.querySelector(".expenses");
 const total = document.querySelector(".total");
 
-let items;
+const pieChartElement = document.querySelector("#pieChart");
+let pieChart;
+
+let items = getItemsFromLocalStorage();
 
 btnNew.onclick = () => {
-  if (descItem.value === "" || amount.value === "" || type.value === "") {
+  if (descItem.value === "" || amount.value === "" || type.value === "" || date.value === "") {
     return alert("Preencha todos os campos!");
   }
 
@@ -19,6 +25,7 @@ btnNew.onclick = () => {
     desc: descItem.value,
     amount: Math.abs(amount.value).toFixed(2),
     type: type.value,
+    date: date.value,
   });
 
   setItemsToLocalStorage();
@@ -27,47 +34,72 @@ btnNew.onclick = () => {
 
   descItem.value = "";
   amount.value = "";
+  date.value = "";
+};
 
-  renderPieChart(); // Atualiza o gráfico após a inclusão de um novo valor
+btnSave.onclick = () => {
+  const data = JSON.stringify(items);
+  downloadFile(data, "data.txt");
+};
+
+btnLoad.onclick = () => {
+  const fileInput = document.createElement("input");
+  fileInput.type = "file";
+  fileInput.accept = ".txt";
+
+  fileInput.onchange = (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = (e) => {
+      const contents = e.target.result;
+      const parsedData = JSON.parse(contents);
+      items = parsedData;
+      setItemsToLocalStorage();
+      loadItems();
+    };
+
+    reader.readAsText(file);
+  };
+
+  fileInput.click();
 };
 
 function deleteItem(index) {
   items.splice(index, 1);
   setItemsToLocalStorage();
   loadItems();
-
-  renderPieChart(); // Atualiza o gráfico após a exclusão de um valor
 }
 
 function insertItem(item, index) {
-  let tr = document.createElement("tr");
+  const tr = document.createElement("tr");
 
   tr.innerHTML = `
     <td>${item.desc}</td>
     <td>R$ ${item.amount}</td>
-    <td class="columnType">${
-    item.type === "Entrada"
-      ? '<i class="bx bxs-chevron-up-circle"></i>'
-      : '<i class="bx bxs-chevron-down-circle"></i>'
-    }</td>
+    <td class="columnType">${item.type === "Entrada" ? '<i class="bx bxs-chevron-up-circle"></i>' : '<i class="bx bxs-chevron-down-circle"></i>'}</td>
+    <td class="columnDate">${item.date}</td>
     <td class="columnAction">
       <button onclick="deleteItem(${index})"><i class='bx bx-trash'></i></button>
     </td>
   `;
 
   tbody.appendChild(tr);
-
-  renderPieChart(); // Atualiza o gráfico após a inclusão de um novo valor
 }
 
 function loadItems() {
-  items = getItemsFromLocalStorage();
   tbody.innerHTML = "";
+  items.sort((a, b) => {
+    const dateA = new Date(a.date);
+    const dateB = new Date(b.date);
+    return dateA - dateB;
+  });
   items.forEach((item, index) => {
     insertItem(item, index);
   });
 
   calculateTotals();
+  updatePieChart();
 }
 
 function calculateTotals() {
@@ -79,68 +111,67 @@ function calculateTotals() {
     .filter((item) => item.type === "Saída")
     .map((transaction) => Number(transaction.amount));
 
-  const totalIncomes = amountIncomes.reduce((acc, item) => acc + item, 0);
-  const totalExpenses = amountExpenses.reduce((acc, item) => acc + item, 0);
+  const totalIncomes = amountIncomes.reduce((acc, cur) => acc + cur, 0).toFixed(2);
+  const totalExpenses = Math.abs(amountExpenses.reduce((acc, cur) => acc + cur, 0)).toFixed(2);
+  const totalItems = (totalIncomes - totalExpenses).toFixed(2);
 
-  incomes.textContent = totalIncomes.toFixed(2);
-  expenses.textContent = totalExpenses.toFixed(2);
-  total.textContent = (totalIncomes - totalExpenses).toFixed(2);
+  incomes.textContent = totalIncomes;
+  expenses.textContent = totalExpenses;
+  total.textContent = totalItems;
 }
 
-function renderPieChart() {
-  const ctx = document.getElementById("pieChart").getContext("2d");
+function updatePieChart() {
+  const amountIncomes = items
+    .filter((item) => item.type === "Entrada")
+    .map((transaction) => Number(transaction.amount));
 
-  const labels = items.map((item) => item.desc);
-  const amounts = items.map((item) => Number(item.amount));
-
-  const colors = [
-    "#FF6384",
-    "#36A2EB",
-    "#FFCE56",
-    "#D9418C",
-    "#4BC0C0",
-    "#FF9F40",
-    "#9966FF",
-    "#FF6384",
-    "#36A2EB",
-    "#FFCE56",
-  ];
+  const amountExpenses = items
+    .filter((item) => item.type === "Saída")
+    .map((transaction) => Number(transaction.amount));
 
   const data = {
-    labels: labels,
+    labels: ["Entradas", "Saídas"],
     datasets: [
       {
-        data: amounts,
-        backgroundColor: colors,
+        data: [
+          amountIncomes.reduce((acc, cur) => acc + cur, 0),
+          amountExpenses.reduce((acc, cur) => acc + cur, 0),
+        ],
+        backgroundColor: ["#00FF00", "#D83121"],
       },
     ],
   };
 
-  const options = {
-    maintainAspectRatio: false,
-  };
-
-  // Remove o gráfico anterior (se existir) antes de renderizar um novo
-  const existingChart = Chart.getChart(ctx);
-  if (existingChart) {
-    existingChart.destroy();
+  if (!pieChart) {
+    pieChart = new Chart(pieChartElement, {
+      type: "pie",
+      data: data,
+      options: {
+        responsive: true,
+      },
+    });
+  } else {
+    pieChart.data = data;
+    pieChart.update();
   }
-
-  new Chart(ctx, {
-    type: "pie",
-    data: data,
-    options: options,
-  });
-}
-
-function setItemsToLocalStorage() {
-  localStorage.setItem("items", JSON.stringify(items));
 }
 
 function getItemsFromLocalStorage() {
-  const localStorageItems = localStorage.getItem("items");
+  return JSON.parse(localStorage.getItem("db_items")) || [];
+}
 
-  return localStorageItems ? JSON.parse(localStorageItems) : [];
+function setItemsToLocalStorage() {
+  localStorage.setItem("db_items", JSON.stringify(items));
+}
+
+function downloadFile(data, filename) {
+  const element = document.createElement("a");
+  element.setAttribute("href", "data:text/plain;charset=utf-8," + encodeURIComponent(data));
+  element.setAttribute("download", filename);
+  element.style.display = "none";
+  document.body.appendChild(element);
+  element.click();
+  document.body.removeChild(element);
 }
 
 loadItems();
